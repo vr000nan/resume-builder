9f903d608937d44fa1402b137c210a76bbd0cdb8
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-03-04 13:17:03 -0500

    routes set up

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import { React, Suspense } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { HomeScreen, Authentication } from "../pages";
 
 const App = () => {
   return (
-    <div className='w-screen flex items-center justify-center'>App TEST</div>
+    // Suspense helps with issues with routing
+    <Suspense fallback={<div>Loading...</div>}>
+      <Routes>
+        <Route path="/*" element={<HomeScreen />} />
+        <Route path="/auth" element={<Authentication/>} />
+      </Routes>
+    </Suspense>
   )
 }
 
