09faf2efe3c774a5b971ed24bb3b8b6e987b917f
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-03-08 12:48:43 -0500

    file select underway

--- src/pages/CreateTemplate.jsx
+++ src/pages/CreateTemplate.jsx
@@ -1,9 +1,29 @@
 import React, { useState } from 'react'
+import { FaUpload } from 'react-icons/fa6';
+import { PuffLoader } from 'react-spinners';
 
 const CreateTemplate = () => {
   const [formData, setFormData] = useState({
-    title:
+    title: "",
+    imageURL: null
   });
+
+  // handling the input field change
+  const handleInputChange = (e) => {
+    const {name, value} = e.target;
+    setFormData((prevRec) => ({...prevRec, [name]: value }));
+  }
+
+  const [imageAsset, setImageAsset] = useState({
+    isImageLoading : false,
+    url : null,
+    progress : 0
+  });
+
+  const handleFileSelect = async(e) => {
+    const file = e.target.files[0];
+    console.log("FILE", file)
+  }
 
   return (
     <div className="w-full px-4 lg:px-10 2xl:px-32 py-4 grid grid-cols-1 lg:grid-cols-12">
@@ -24,11 +44,58 @@ const CreateTemplate = () => {
         </div>
 
         {/* template title section */}
-        <input type="text"/>
+        <input 
+        className="w-full px-4 py-3 rounded-md bg-transparent border border-gray-300 text-txtPrimary focus:text-txtDark focus:shadow-md outline-nons"
+        type="text" 
+        name="title"
+        placeholder="Template Title" 
+        value={formData.title} 
+        onChange={handleInputChange} 
+        />
+
+        {/* file uploader section */}
+        <div className="w-full bg-gray-100 backdrop-blur-md h-[420px] lg:h-[620px} 2xl:h-[740px] rounded-md border-2 border-dotted border-gray-300 cursor-pointer flex items-center justify-center">
+          {imageAsset.isImageLoading ? (
+          <React.Fragment>
+            <div className="flex flex-col items-center justify-center gap-4">
+              <PuffLoader color="#498FCD" size ={40}/>
+              <p>{imageAsset?.progress.toFixed(2)}%</p>
+            </div>
+          </React.Fragment>
+          ) : (
+          <React.Fragment>
+            {!imageAsset?.url ? (
+            <React.Fragment>
+              <label className="w-full cursor-pointer h-full">
+                <div className="flex flex-col items-center justify-center h-full w-full">
+                  <div className="flex flex-col items-center justify-center cursor-pointer gap-4">
+                      <FaUpload className="text-2xl" /> 
+                      <p className="text-lg text-txtLight">Click to Upload</p>
+                  </div>
+                </div>
+
+                <input
+                  type="file"
+                  className="w-0 h-0"
+                  accept=".jpeg,.jpg,.png"
+                  onChange={handleFileSelect}
+                 />
+              </label>
+            </React.Fragment>
+            ): (
+              <React.Fragment>
+
+              </React.Fragment>
+            )}
+          </React.Fragment>
+          )}
+        </div>
       </div>
 
+
+
       {/* right container */}
-      <div className="col-span-12 lg:col-span-8 2xl:col-span-9 bg-red-200">Right Side</div>
+      <div className="col-span-12 lg:col-span-8 2xl:col-span-9">Right Side</div>
     </div>
   )
 }
