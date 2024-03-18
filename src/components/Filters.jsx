07c0402931da605a9e01e3025179e8d99513db18
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-03-18 14:01:57 -0400

    fixed clear all issue

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -39,6 +39,7 @@ const Filters = () => {
       className="border border-gray-300 rounded-md px-3 py-2 mr-2 cursor-pointer group hover:shadow-md bg-gray-200 relative"
       onMouseEnter={() => setIsClearHovered(true)}
       onMouseLeave={() => setIsClearHovered(false)}
+      onClick={clearFilter}
       >
         <MdLayersClear className="text-xl" />
 
@@ -47,7 +48,6 @@ const Filters = () => {
            <motion.div 
            {...slideUpDownWithScale}
            className="absolute -top-8 -left-2 bg-white shadow-md rounded-md px-2 py-1"
-           onClick={clearFilter}
            >
              <p className="whitespace-nowrap text-xs">Clear All</p>
            </motion.div>
