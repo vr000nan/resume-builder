3910738e522c77bd489ab333d0e7fb8dc340da94
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-03-19 13:45:49 -0400

    scale animations added to thumbnails

--- src/components/TemplateDesignPin.jsx
+++ src/components/TemplateDesignPin.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { motion } from 'framer-motion';
+import { scaleInOut } from '../animations';
 
-const TemplateDesignPin = () => {
+const TemplateDesignPin = ({ data, index }) => {
   return (
-    <div>TemplateDesignPin</div>
+    <motion.div 
+    key={data?._id}
+    {...scaleInOut(index)}
+    >
+        <div className="w-full h-[500px] rounded-md bg-gray-200 overflow-hidden relative">
+            <img 
+            src={data?.imageURL} 
+            alt="image url" 
+            className="w-full h-full object-cover"
+            />
+        </div>
+    </motion.div>
   )
 }
 
-export default TemplateDesignPin
+export default TemplateDesignPin;
