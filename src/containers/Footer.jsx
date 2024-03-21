4da054c7db080c18543d12884c33d2a01628bb2c
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-03-21 12:40:48 -0400

    fixed footer, setting up templatePinDetails

--- src/containers/Footer.jsx
+++ src/containers/Footer.jsx
@@ -10,9 +10,9 @@ const Footer = () => {
             <p>Vrésumé</p>
         </div>
         <div className="flex items-center justify-center gap-6">
-            <Link to={"/"} className="text-blue-700 text-sm">Home</Link>
-            <Link to={"/"} className="text-blue-700 text-sm">Contact</Link>
-            <Link to={"/"} className="text-blue-700 text-sm whitespace-nowrap">Privacy Policy</Link>
+            <Link to={"https://github.com/vr000nan/resume-builder"} className="text-blue-700 text-sm">Home</Link>
+            <Link to={"https://nanimal.dev"} className="text-blue-700 text-sm">Contact</Link>
+            <Link to={"https://www.linkedin.com/in/nan-wroblewski-09547a224/"} className="text-blue-700 text-sm whitespace-nowrap">Privacy Policy</Link>
         </div>
     </div>
   )
--- src/pages/TemplateDesignPinDetails.jsx
+++ src/pages/TemplateDesignPinDetails.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { useQuery } from 'react-query';
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { getTemplateDetails } from '../api';
 import { MainSpinner } from "../components"
+import { FaHouse } from 'react-icons/fa6';
 
 const TemplateDesignPinDetails = () => {
   const { templateID } = useParams();
@@ -14,9 +15,37 @@ const TemplateDesignPinDetails = () => {
 
   if(isLoading) return <MainSpinner />
 
-  return (
-    <div>TemplateDesignPinDetails</div>
-  )
+  if(isError){
+    return (
+      <div className="w-full h-[60vh] flex fex-col items-center justify-center">
+        <p className="text-lg text-txtPrimary font-semibold">Error while fetching the data... Please try again later!</p>
+      </div>
+    );
+  };
+
+  return(
+    <div className="w-full flex items-center justify-start flex-col px-4 py-12">
+      {/* bread crumb */}
+      <div className="w-full flex items-center pb-8 gap-2">
+        <Link
+        to={"/"}
+        className="flex items-center justify-center gap-2 text-txtPrimary"
+        >
+          <FaHouse /> Home
+        </Link>
+        <p>/</p>
+        <p>{data.name}</p>
+      </div>
+
+     {/* design main section layout */}
+     <div className="w-full grid grid-cols-1 lg:grid-cols-12">
+      {/* left section */}
+      <div className="col-span-1 lg:col-span-8">1</div>
+      {/* right section */}
+      <div className="col-span-1 lg:col-span-4">2</div>
+     </div>
+    </div>
+  );
 }
 
 export default TemplateDesignPinDetails
