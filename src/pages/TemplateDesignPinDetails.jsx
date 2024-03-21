53031e77e31b0da8603143c94452a88c0e393179
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-03-21 14:46:01 -0400

    template image, likes, and add button added

--- src/pages/TemplateDesignPinDetails.jsx
+++ src/pages/TemplateDesignPinDetails.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { useQuery } from 'react-query';
 import { Link, useParams } from 'react-router-dom';
-import { getTemplateDetails } from '../api';
+import { getTemplateDetails, saveToCollections } from '../api';
 import { MainSpinner } from "../components"
 import { FaHouse } from 'react-icons/fa6';
+import { BiFolderPlus, BiHeart, BiSolidFolderPlus } from 'react-icons/bi';
+import useUser from '../hooks/useUser';
 
 const TemplateDesignPinDetails = () => {
   const { templateID } = useParams();
@@ -12,6 +14,14 @@ const TemplateDesignPinDetails = () => {
     ["template", templateID],
     () => getTemplateDetails(templateID)
   );
+
+  const { data:user, refetch: userRefetch } = useUser();
+
+  const addToCollection = async (e) => {
+    e.stopPropagation();
+    await saveToCollections(user, data);
+    userRefetch();
+  };
 
   if(isLoading) return <MainSpinner />
 
@@ -40,7 +50,68 @@ const TemplateDesignPinDetails = () => {
      {/* design main section layout */}
      <div className="w-full grid grid-cols-1 lg:grid-cols-12">
       {/* left section */}
-      <div className="col-span-1 lg:col-span-8">1</div>
+      <div className="col-span-1 lg:col-span-8 flex-col items-start justify-start gap-4">
+        {/* load */}
+        <img 
+        className="w-full h-auto object-contain rounded-md"
+        src={data?.imageURL} 
+        alt="Template Image"
+        />
+
+        {/* title and other options */}
+        <div className="w-full flex flex-col items-start justify-start gap-2">
+          {/* title section */}
+          <div className="w-full flex items-center justify-between">
+            {/* title */}
+            <p className="text-base text-txtPrimary font-semibold">
+              {data?.title}
+            </p>
+
+            {/* likes */}
+            {data?.favorites?.length > 0 && (
+            <div className="flex items-center justify-center gap-1">
+              <BiHeart className="text-base text-red-500" />
+              <p className="text-base text-txtPrimary font-semibold">{data?.favorites?.length} likes</p>
+            </div>
+            )}
+
+            {/* collection favorite options */}
+            {user && (
+              <div className="flex items-center justify-center gap-3">
+                {user?.collections?.includes(data?._id) ? 
+                <React.Fragment>
+                  <div 
+                  onClick={addToCollection}
+                  className="flex items-center justify-center px-4 py-2 rounded-md border border-gray-200 cursor-pointer"
+                  >
+                    <BiSolidFolderPlus className="text-base text-txtPrimary" />
+                    <p 
+                    className="text-sm text-txtPrimary whitespace-nowrap"
+                    >
+                      Remove From Collections
+                    </p>
+                  </div>
+                </React.Fragment>
+                :
+                <React.Fragment>
+                <div 
+                onClick={addToCollection}
+                className="flex items-center justify-center px-4 py-2 rounded-md border border-gray-200 cursor-pointer"
+                >
+                  <BiFolderPlus className="text-base text-txtPrimary" />
+                  <p 
+                  className="text-sm text-txtPrimary whitespace-nowrap"
+                  >
+                    Add To Collections
+                  </p>
+                </div>
+                </React.Fragment>
+                }
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
       {/* right section */}
       <div className="col-span-1 lg:col-span-4">2</div>
      </div>
