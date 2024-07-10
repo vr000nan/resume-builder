5be8d0461abca44692c32d67f99f704025f4dfb7
Nan <85908109+HannahWroblewski@users.noreply.github.com>
2024-07-10 19:09:52 -0400

    nvm

--- src/components/AuthButtonWithProvider.jsx
+++ src/components/AuthButtonWithProvider.jsx
@@ -1,45 +1,48 @@
-import React from 'react';
+import React from 'react'
 import { FaChevronRight } from 'react-icons/fa6';
 import { GoogleAuthProvider, GithubAuthProvider, signInWithRedirect } from "firebase/auth";
 import { auth } from "../config/firebase.config";
 
-const AuthButtonWithProvider = ({ Icon, label, provider }) => {
+const AuthButtonWithProvider = ({Icon, label, provider}) => {
     const googleAuthProvider = new GoogleAuthProvider();
     const githubAuthProvider = new GithubAuthProvider();
 
     const handleClick = async () => {
-        let authProvider;
-
-        switch (provider) {
+        switch(provider){
             case "GoogleAuthProvider":
-                authProvider = googleAuthProvider;
+                await signInWithRedirect(auth, googleAuthProvider).then((result) => {
+                    console.log(result)
+                }).catch((err) => {
+                    console.log(`*ERROR!* ${err.Message}`);
+                });
                 break;
 
             case "GithubAuthProvider":
-                authProvider = githubAuthProvider;
+                await signInWithRedirect(auth, githubAuthProvider).then((result) => {
+                    console.log(result)
+                }).catch((err) => {
+                    console.log(`*ERROR!* ${err.Message}`);
+                });
                 break;
-
+            
             default:
-                authProvider = googleAuthProvider;
+                await signInWithRedirect(auth, googleAuthProvider).then((result) => {
+                    console.log(result)
+                }).catch((err) => {
+                    console.log(`*ERROR!* ${err.Message}`);
+                });
                 break;
-        }
-
-        try {
-            await signInWithRedirect(auth, authProvider);
-            console.log("Sign in successful!");
-            // Additional actions after successful sign-in
-        } catch (err) {
-            console.error("*ERROR!*", err.message); // Correctly accessing error message
         }
     };
 
-    return (
-        <div onClick={handleClick} className="w-full px-4 py-3 rounded-md border-2 border-blue-700 flex items-center justify-between cursor-pointer group hover:bg-blue-700 active:scale-95 duration-150 hover:shadow-md">
-            <Icon className="text-txtPrimary text-xl group-hover:text-white"/>
-            <p className="text-txtPrimary text-lg group-hover:text-white">{label}</p>
-            <FaChevronRight className="text-txtPrimary text-base group-hover:text-white" />
-        </div>
-    );
-};
+
+  return (
+    <div onClick={handleClick} className="w-full px-4 py-3 rounded-md border-2 border-blue-700 flex items-center justify-between cursor-pointer group hover:bg-blue-700 active:scale-95 duration-150 hover:shadow-md">
+        <Icon className="text-txtPrimary text-xl group-hover:text-white"/>
+        <p className="text-txtPrimary text-lg group-hover:text-white">{label}</p>
+        <FaChevronRight className="text-txtPrimary text-base group-hover:text-white" />
+    </div>
+  )
+}
 
 export default AuthButtonWithProvider;
